bf6fb00c579b348b80b5a0c17d6cb9011e9b04f3
Pitt009 <gmartinez28837@gmail.com>
2017-04-11 11:58:38 -0500

    Aun falta una cancha

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var botonFutbol = document.getElementById('buttonFutbol');
+botonFutbol.addEventListener("click", mostrarCanchaFut);
+
+function mostrarCanchaFut() {
+   var canchaFutbol = document.getElementById("canchaFutbol");
+   canchaFutbol.style.display = "block";
+}
+/* var botonBasketbol = document.getElementById('buttonBasketbol');
+ botonBasketbol.addEventListener("click",mostrarCanchaBasket);
+
+ function mostrarCanchaBasket() {
+   var canchaBasquetbol = document.getElementById('canchaBasquetbol');
+   canchaBasquetbol.style.display = "block";
+
+ }
+*/
