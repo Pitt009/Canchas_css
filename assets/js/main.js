0715ac7f67cec3aee6e490b6a78209792b5a86f0
Pitt009 <gmartinez28837@gmail.com>
2017-04-11 15:26:53 -0500

    Falta cancha tenis

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,4 @@ botonFutbol.addEventListener("click", mostrarCanchaFut);
 function mostrarCanchaFut() {
    var canchaFutbol = document.getElementById("canchaFutbol");
    canchaFutbol.style.display = "block";
-}
-/* var botonBasketbol = document.getElementById('buttonBasketbol');
- botonBasketbol.addEventListener("click",mostrarCanchaBasket);
-
- function mostrarCanchaBasket() {
-   var canchaBasquetbol = document.getElementById('canchaBasquetbol');
-   canchaBasquetbol.style.display = "block";
-
  }
-*/
